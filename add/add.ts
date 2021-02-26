57b9c7389103630850088890e42674784d2d37d8
Hans Thiel <hsthiel@gmail.com>
2021-02-26 00:45:48 -0300

    Challenge Add

--- add/add.ts
+++ add/add.ts
@@ -1,12 +1,22 @@
 export function add(param1: number, param2: number): number {
-
+    return param1+param2;
 }
 
 export function add2(...param1: number[]): number {
+    var total=0;
+    for (var i =0 ; i<arguments.length ; i++) {
+        total += arguments[i];
+    }
 
+    return total;
 }
-// console.log(add(1, 2));
+
+
+
+console.log(add(1, 2));
 // console.log(add(3, 2));
 
 // console.log(add2(1,2,3,4,5));
 // console.log(add2(2,3));
+
+
